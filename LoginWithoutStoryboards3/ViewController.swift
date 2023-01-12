c7d840076b4647b6c3543b8afe1f2f90f5c29eea
Seneca242 <flabbergasted242@gmail.com>
2023-01-12 18:02:45 +0200

    Initial Commit

---
+++ LoginWithoutStoryboards3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginWithoutStoryboards3
+//
+//  Created by Дмитрий Дмитрий on 12.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
