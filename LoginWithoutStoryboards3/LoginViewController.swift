95b51eab0c561a93c6aeca4a9d0ecb8ac4cd4018
Seneca242 <flabbergasted242@gmail.com>
2023-01-17 19:16:22 +0200

    Tuesday commit

--- LoginWithoutStoryboards3/LoginViewController.swift
+++ LoginWithoutStoryboards3/LoginViewController.swift
@@ -12,13 +12,15 @@ class LoginViewController: UIViewController {
     private let userName = "Dima"
     private let password = "123"
     
+    let personInfo = User.getUserInfo()
+    
     private lazy var stackView: UIStackView = {
         let stackView = UIStackView()
         stackView.axis = .vertical
         stackView.alignment = .fill
         stackView.distribution = .fill
         stackView.spacing = 27
-        stackView.backgroundColor = .red
+//        stackView.backgroundColor = .red
         return stackView
     }()
     
@@ -28,7 +30,7 @@ class LoginViewController: UIViewController {
         stackView.alignment = .fill
         stackView.distribution = .fillEqually
         stackView.spacing = 8
-        stackView.backgroundColor = .yellow
+//        stackView.backgroundColor = .yellow
         return stackView
     }()
     
@@ -52,7 +54,7 @@ class LoginViewController: UIViewController {
         button.configuration = .bordered()
         button.titleLabel?.font = UIFont(name: "Arial", size: 15)
         button.setTitleColor(.blue, for: .normal)
-        button.backgroundColor = .orange
+//        button.backgroundColor = .orange
         button.addTarget(self, action: #selector(login), for: .touchUpInside)
         return button
     }()
@@ -61,7 +63,7 @@ class LoginViewController: UIViewController {
         let button = UIButton()
         button.setTitle("Forgot UserName?", for: .normal)
         button.setTitleColor(.blue, for: .normal)
-        button.backgroundColor = .orange
+//        button.backgroundColor = .orange
         button.titleLabel?.font = UIFont(name: "Arial", size: 15)
         button.addTarget(self, action: #selector(forgotName), for: .touchUpInside)
         return button
@@ -72,7 +74,7 @@ class LoginViewController: UIViewController {
         button.setTitle("Forgot Password?", for: .normal)
         button.setTitleColor(.blue, for: .normal)
         button.titleLabel?.font = UIFont(name: "Arial", size: 15)
-        button.backgroundColor = .orange
+//        button.backgroundColor = .orange
         button.addTarget(self, action: #selector(forgotPass), for: .touchUpInside)
         return button
     }()
@@ -85,6 +87,25 @@ class LoginViewController: UIViewController {
     }
 
 
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(
+            title: title,
+            message: message,
+            preferredStyle: .alert
+        )
+        
+        let okAction = UIAlertAction(
+            title: "Ok",
+            style: .default
+        )
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    
+    
+    
     private func setupView() {
         view.backgroundColor = .white
         addSubviews(subViews: stackView)
@@ -131,15 +152,41 @@ class LoginViewController: UIViewController {
     }
     
     @objc private func login() {
-        
+        if userNameTF.text != personInfo.userName || passwordTF.text != personInfo.password {
+            showAlert(
+                title: "Wrong UserName or Password",
+                message: "Enter correct User Name and Password"
+            )
+        } else {
+            let welcomeVC = WelcomeViewController()
+            let userInfoVC = UserInfoViewController()
+            
+            let navigationUserInfoVC = UINavigationController(rootViewController: userInfoVC)
+            welcomeVC.modalPresentationStyle = .fullScreen
+            present(welcomeVC, animated: true)
+            
+            let tabbar = UITabBarController()
+            welcomeVC.tabBarItem = .init(title: "Welcome", image: UIImage(named: "welcome"), tag: 0)
+            navigationUserInfoVC.tabBarItem = .init(title: "Seneca", image: UIImage(named: "seneca"), selectedImage: nil)
+            tabbar.modalPresentationStyle = .fullScreen
+            tabbar.viewControllers = [welcomeVC, navigationUserInfoVC]
+            
+            present(tabbar, animated: true)
+        }
     }
     
     @objc private func forgotName() {
-        
+        showAlert(
+            title: "Oooops",
+            message: "Your User Name is \(userName)"
+        )
     }
     
     @objc private func forgotPass() {
-        
+        showAlert(
+            title: "Oooops",
+            message: "Your Password is \(password)"
+        )
     }
     
 }
---
+++ LoginWithoutStoryboards3/WelcomeViewController.swift
@@ -0,0 +1,21 @@
+//
+//  WelcomeViewController.swift
+//  LoginWithoutStoryboards3
+//
+//  Created by Дмитрий Дмитрий on 16.01.2023.
+//
+
+import UIKit
+
+class WelcomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    
+
+}
