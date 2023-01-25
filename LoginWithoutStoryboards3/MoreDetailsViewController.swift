812dcbe7e9ca84dcb23278de5dbe3a9c158fbbd7
Seneca242 <flabbergasted242@gmail.com>
2023-01-25 23:34:33 +0200

    tried to make photo round

--- LoginWithoutStoryboards3/MoreDetailsViewController.swift
+++ LoginWithoutStoryboards3/MoreDetailsViewController.swift
@@ -12,8 +12,12 @@ class MoreDetailsViewController: UIViewController {
     var person: User?
     
     private lazy var senecaPhoto: UIImageView = {
-        let image = UIImageView()
-        image.image = UIImage(named: "Seneca")
+        var image = UIImageView()
+        image = UIImageView(frame:CGRect(x: 50, y: 200, width: 300, height: 300))
+        image.image = UIImage(named: person?.person?.personPhoto ?? "")
+        image.backgroundColor = .orange
+        image.contentMode = .scaleAspectFit
+        image.layer.cornerRadius = image.frame.width / 2
         return image
     }()
     
@@ -23,13 +27,24 @@ class MoreDetailsViewController: UIViewController {
         view.addSubview(senecaPhoto)
     }
     
+//    override func viewDidLayoutSubviews() {
+//        super.viewDidLayoutSubviews()
+//        changeCornerRadius()
+//    }
+//
+//    func changeCornerRadius() {
+//        senecaPhoto.layer.cornerRadius = senecaPhoto.frame.width / 2
+//    }
+    
     private func setupConstraints() {
         senecaPhoto.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             senecaPhoto.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            senecaPhoto.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -100),
             senecaPhoto.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            senecaPhoto.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            senecaPhoto.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            senecaPhoto.heightAnchor.constraint(equalToConstant: 300)
             
         ])
     }
